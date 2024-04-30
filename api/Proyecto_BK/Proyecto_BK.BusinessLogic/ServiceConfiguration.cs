a1b21969ad94abf368b30f504a9284fbfae1619a
Dany <145422491+DanyFranc0@users.noreply.github.com>
2024-04-30 00:03:34 -0600

    API y plantilla [Sin terminar]

---
+++ api/Proyecto_BK/Proyecto_BK.BusinessLogic/ServiceConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Proyecto_BK.DataAccess;
+//using Proyecto_BK.DataAccess.Repository;
+using Proyecto_BK.BusinessLogic.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_BK.BusinessLogic.Services
+{
+    public static class ServiceConfiguration
+    {
+        public static void DataAccess(this IServiceCollection service, string conn)
+        {
+
+
+            Proyecto_BKContext.BuildConnectionString(conn);
+        }
+        public static void BusinessLogic(this IServiceCollection service)
+        {
+
+        }
+    }
+}
---
+++ api/Proyecto_BK/Proyecto_BK.DataAccess/Proyecto_BKContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_BK.DataAccess
+{
+    public class Proyecto_BKContext : DbsistemarestaurantebkContext
+    {
+        public static string ConnectionString { get; set; }
+
+        public Proyecto_BKContext()
+        {
+            ChangeTracker.LazyLoadingEnabled = false;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ConnectionString);
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public static void BuildConnectionString(string connection)
+        {
+            var connectionStringBuilder = new SqlConnectionStringBuilder { ConnectionString = connection };
+            ConnectionString = connectionStringBuilder.ConnectionString;
+        }
+    }
+}
